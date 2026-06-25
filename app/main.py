053a47b354b7c85be819e96c1e074413abe7cdeb
Nik <nikita.pitushkin31@mail.com>
2026-06-25 17:15:41 +0300

    Add Docker weather service

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import os
+import requests
+
+API_KEY = os.environ.get("API_KEY")
+URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {
+        "key": API_KEY,
+        "q": CITY,
+    }
+    response = requests.get(URL, params)
+    data = response.json()
+
+    city = data["location"]["name"]
+    temp = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+    print(
+        f"Weather in {city}: "
+        f"{temp}°C, "
+        f"{condition}"
+    )
 
 
 if __name__ == "__main__":
